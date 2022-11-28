d74ec1e802b41f0cbd101a7751f387efa8736415
Dmitri <68437321+Stalkerov123@users.noreply.github.com>
2022-11-28 19:43:42 +0300

    Update 12_1_Б.py

--- 12_1_Б.py
+++ 12_1_Б.py
@@ -1,18 +1,8 @@
 def main():
-    mx = -100500
-    a, n = [], int(input('Кол-во элементов последовательности: '))
-    for _ in range(n):
-        a.append(int(input()))
-    a.append(0)
-    
-    if len(a) == 0:
+    n = int(input())
+    if n == 0:
         return 0
     else:
-        for i in range(len(a)-1):
-            if a[i] > mx:
-                mx = a[i]
-        print(f"Максимальный элемент последовательности: {mx}")
+        return max(n, main())
     
-main()
-
-
+print(main())
